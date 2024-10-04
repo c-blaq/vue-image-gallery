d520d82e46664fd22d386a46535d921b51d8606c
c-blaq <babayejuadesina@gmail.com>
2024-10-04 12:41:56 +0100

    feat: search image

---
+++ pages/search/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1 v-if="photos?.results.length === 0">
+    Search result for {{ query.q }} not found!
+  </h1>
+  <PhotoGrid v-else :photos="photos?.results" />
+</template>
+
+<script setup lang="ts">
+import PhotoGrid from "@/components/PhotoGrid.vue";
+import { useSearchPhoto } from "@/api/photo";
+const route = useRoute();
+const { query } = route;
+const { data: photos, status } = useSearchPhoto(query.q as string);
+</script>
+
+<style lang="scss" scoped>
+h1 {
+  text-align: center;
+  margin-top: 2rem;
+}
+</style>
