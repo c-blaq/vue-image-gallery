b4dfbe10f3930d720e0c2d36d5739c4272bd45d6
c-blaq <babayejuadesina@gmail.com>
2024-10-04 10:15:08 +0100

    style: searchbar ui

--- components/SearchBar.vue
+++ components/SearchBar.vue
@@ -3,6 +3,7 @@
     <div class="searchIcon-container">
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
         <path
+          fill="#253858"
           d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"
         />
       </svg>
@@ -18,7 +19,7 @@
 
 <script setup lang="ts"></script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 form {
   width: 100%;
   max-width: 1158px;
@@ -29,8 +30,9 @@ form {
     width: 1.25rem;
     height: 1.25rem;
     position: absolute;
-    top: 40%;
+    bottom: 40px;
     left: 1.5rem;
+    transform: translateY(50%);
   }
 
   & > .search-input {
@@ -42,6 +44,22 @@ form {
     border-radius: 0.8rem;
     padding: 0 1rem 0 4rem;
     box-shadow: 0px 0px 1px gray;
+    font-size: 1.2rem;
+    color: #253858;
+
+    &::placeholder {
+      color: #253858;
+    }
+  }
+
+  @media (max-width: 768px) {
+    & > .searchIcon-container {
+      bottom: 25px;
+    }
+
+    & > .search-input {
+      height: 50px;
+    }
   }
 }
 </style>
